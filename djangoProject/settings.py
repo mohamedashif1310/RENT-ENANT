86957d859d672791c43ec7ab42e878ad7b3b32a9
mohamedashif1310 <mohamedashif1310@gmail.com>
2024-05-25 13:12:24 +0530

    updated project with latest changes

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from users.models import CustomUser
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
+from users.models import CustomUser
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- manage.py
+++ manage.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+# manage.py
 import os
 import sys
-
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
     try:
         from django.core.management import execute_from_command_line
+        import django
+        django.setup()
+        print("Django setup completed.")  # Debugging statement
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -16,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
